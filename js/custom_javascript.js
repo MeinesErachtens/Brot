ef50a0c1be3a7dcad7a0e7c8a42a579aeec54f51
MeinesErachtens <s143527@hft-leipzig.de>
2016-11-23 09:16:40 +0100

    zähler fertig

--- js/custom_javascript.js
+++ js/custom_javascript.js
@@ -1,49 +1,61 @@
-var string = document.getElementById("time_gray").innerHTML;
-var time_array = string.match(/[0-9]+/);
-var time = time_array[0]*60;
+var string;
+var interval = [];
+var time_array;
+var time = [];
 var initialOffset = '440';
-var i = time;
-var flag = false;
+var i = [];
+var flag = [];
 var audio = new Audio('audio/output.mp3');
 
 
-function countdown () {
+function countdown ( name ) {
+	string = document.getElementById("time_"+name).innerHTML;
+	time_array = string.match(/[0-9]+/);
+	time[name] = time_array[0]*60;
+	i[name] = time[name];
 	
-	console.log();
-	if( flag === true ) {
-		flag = false;
+	console.log("countdown function:" +name);
+	if( flag[name] === true ) {
+		stop( name );
 		return;
 	}
-	flag = true;
-	start();
+	flag[name] = true;
+	start( name );
+	return;
 }
 
-function start() {
-	var interval = setInterval(function() {
+function start( name ) {
+	interval[name] = setInterval(function() {
+		var coun = i[name];
+		console.log(interval);
+		console.log(i);
 		// click on running counter to stop it
-		if( flag === false ) {
-			console.log("hi");
-			clearInterval(interval);
-			audio.pause	();
-			i = time;
-			$('#timer').text("Start");
-			$("#circle_gray").css('stroke-dashoffset', 0);
+		if( flag[name] === false ) {
+			stop(name);
 			return;
 		}
-		$('#timer').text( dig_time(i) );
-		if (i == 0) {  	
-			clearInterval(interval);
-			
-			
+		$('#timer_'+name).text( dig_time( coun ) );
+		if ( coun == 0) {  	
+			clearInterval(interval[name]);
 			audio.play();
-			
-			
 			return;
-			
 		}
-		$('#circle_gray').css('stroke-dashoffset', initialOffset-((i-1)*(initialOffset/time)));
-		i--;  
+		$('#circle_'+name).css('stroke-dashoffset', initialOffset-((coun-1)*(initialOffset/time[name])));
+		i[name] = coun-1;
+		console.log(i);		
 	}, 1000);
+}
+
+function stop( name ) {
+	clearInterval(interval[name]);
+	flag[name] = false;
+	interval.splice( name, 1 );
+	
+	audio.pause();
+	i[name] = time[name];
+	$('#timer_'+name).text("Start");
+	$("#circle_"+name).css('stroke-dashoffset', 0);
+	return;
 }
 
 function dig_time( sec ) {
