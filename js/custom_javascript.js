7e001915ccbd110da843c114895a7674eb8ff4f6
MeinesErachtens <s143527@hft-leipzig.de>
2016-11-23 11:51:10 +0100

    einiges

--- js/custom_javascript.js
+++ js/custom_javascript.js
@@ -5,7 +5,7 @@ var time = [];
 var initialOffset = '440';
 var i = [];
 var flag = [];
-var audio = new Audio('audio/vivi_jingle');
+var audio = new Audio('audio/BrotistFertigRemix.ogg');
 
 
 function countdown ( name ) {
